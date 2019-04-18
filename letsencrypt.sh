3aa81bc006254173ce87faf12ac5c3c066cb3196
app2linux <46576676+app2linux@users.noreply.github.com>
2019-04-18 09:43:42 +0200

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -9,56 +9,87 @@ lineOrder="${@} "
 #  ---------------------------------------------------------
 echo
 echo $LINE$LINE
-echo -e "\nStart install & config SSL Letsencrypt for a Nginx Docker server."
+echo -e "\nStart download SSL Letsencrypt for a Nginx Docker server."
 echo $LINE
 #  ---------------------------------------------------------
-function isContinue(){
-echo
-echo -n "[x] Cancel & break     [c] Continue   > "
-while true; do  read -rsn1  input
-   case $input in 
-	[cC]) { echo;break;} ;;
-	[xX]) { echo;exit 0;} ;; 
- 	esac
-done
+function isOk(){
+echo -en "  ::  It's OK?  >  "; 
+while IFS= read -rsn1 key; do
+    [[ $key =~ ^[YySs]$ ]] && return 1
+    [[ $key =~ ^[Nn]$ ]] && return 0
+    done
 return 1
 }
 #  ---------------------------------------------------------
+echo -e 'Download Letsencrypt certificates ...\n\n'$LINE
+ssl=$(sed -e '/^export SSL=/ !d' context.sh)
+[[ ${ssl:11:1} == '"' || ${ssl:11:1} == "'" ]] && ssl=${ssl:12:-1} || ssl=${ssl:11}
+if [ ! -z $ssl ]; then
+    echo -e "\n\nSSL/TLS already configured with $ssl\n\n"$LINE 
+    echo -en "\t"; read -rsn1 -p "Press a key to continue  >  " key; exit;
+fi
+source ./context.sh
+domains=''
+for ((i=1; i<${#context[@]}; i++)); do
+    data=(${context[i]:1:-1})
+    [[ -z $data || $data == "''" ]] && break
+    domains+=${data[0]}" "
+    done
+echo -en "Domains:\n\t{"$domains"}"
+if [ -z $domains ]; then
+    echo -e "\nNo domains to include into certificate\n"$LINE 
+    echo -en "\t"; read -rsn1 -p "Press a key to continue  >  " key; exit;
+fi
+#
+clear
+echo -e "\n"$LINE$LINE"\nDefine LetsEncrypt options:\n"$LINE 
+echo "    [<myEmail>] [auto] [sslOff]"
+echo
+echo "LetsEncrypt registration email is optional"
+echo "[auto] Autorenew certificate"
+echo "[sslOff] Download ssl certificate but do not web configure"
+echo "All domains are included in certificate"
+echo "    {"$domains"}"
+echo -e "\n    [x]  Continue without ssl config"
+echo -e $LINE 
+read -p "   > " lineOrder
+lineOrder=" "${lineOrder,,}" "
+[[ $lineOrder == *" x "* || $lineOrder == *" c "* ]] && { clear; exit; }
+[[ $lineOrder == *" auto "* ]] && autoRenew=1 || autoRenew=0
+if [[ ${lineOrder} = " "*"@"*" " ]]; then 
+    email1=${lineOrder%@* };email1=${email1##* }; 
+    email=${lineOrder//*@/}; email=$email1@${email// */};
+    if [[ ! "$email" =~ [a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4} ]]; then
+        echo -e "\nIncorrect email format\n\n"$LINE 
+        echo -en "\t"; read -rsn1 -p "Press a key to continue  >  " key; exit;
+    fi
+else
+    email=''
+fi
+echo -en "\n"$LINE"\nDomains: {"$domains"}\n  eMail: <"$email">\n"
+[[ $autoRenew == 1 ]] && echo -e "Autorenew certificate. \n\tTesting all weeks." || echo -e "Manual renewal of certificates. \n\tNotice by email."
+echo -en $LINE"\nCertificate data"
+isOk; val=$?; [[ $val == 0 ]] && exit
+#  ---------------------------------------------------------
+# installa certificat
 [[ ! -f ./certbot-auto ]] &&  { wget https://dl.eff.org/certbot-auto -P ./; chmod +x ./certbot-auto; }
 [[ ! -f ./letsencrypt-auto ]] && { wget https://github.com/certbot/certbot/raw/master/letsencrypt-auto -P ./; chmod +x ./letsencrypt-auto; }
 if [[ -d /etc/letsencrypt/live ]]; then
     echo -e "\nLetsEncrypt certificates already configured\n\n"$LINE
-    read -rsn1 -p "Press any key to continue > "
-    exit 0
+    read -rsn1 -p "Press any key to continue > "; exit 0;
 fi
-[[ ${lineOrder} = *"email="* ]] && { email=${lineOrder//*email=/};email=${email// */}; } || email=''
-echo 'email  : '$email
-[[ ${lineOrder} = *"domains="* ]] && { domains=${lineOrder//*domains=[\{|\[]/};domains=${domains//[\}|\]]*/}; } || domains=''
-domainList=${domains//,/ };domains=${domains//  / };
-domains=${domains//,/ };domains=${domains//  / };
-domains=${domains// /, };
-echo 'Domains: '${domains[@]}
-echo -en "Config SSL domains:"; [[ ${lineOrder} = *"sslon"* ]] && echo 'ssl ON' || echo 'ssl OFF'
-echo $LINE$LINE
-if [[ ! ${domains[@]} ]]; then
-    echo -e "   >  Error: No domains declared ...\n"$LINE$LINE"\n\t"
-    read -rsn1 -p "  Press any key to continue > "
-    echo
-    exit 0
-fi
-#
 file='/etc/letsencrypt/cli.ini'
-echo -e "\n"$file 'content\n'$LINE
+echo -e "\n\n"$file 'content\n'$LINE
 content="rsa-key-size = 4096"
-[[ ! ${email} ]] && content+="\nemail = "$email || "\n#email = "
+[[ ${email} ]] && content+="\nemail = "$email || content+="\n#email = "
 content+="\npreferred-challenges = http-01\nagree-tos = True\nrenew-by-default = True\n"
 content+="domains = "$domains
 echo -e $content
 echo $LINE$LINE
 echo -e "\nStarting install LetsEncrypt certificate ..."
-isContinue
-#echo -e $content > $file
-echo $LINE$LINE
+echo -e $content > $file
+echo $LINE
+docker stop nginx
 #./letsencrypt-auto certonly --standalone
 ./certbot-auto certonly --standalone
 echo
@@ -73,8 +104,16 @@ if [[ -d /etc/letsencrypt/live ]]; then
     echo $temp
     sed -Ei "s/^export SSLdomains.*$/$temp/g" context.sh
 fi
-[[ ${lineOrder} = *"sslon"* ]] && ./sslConfig.sh || echo 'ssl not configured on domains'
-echo -en "\n\t"
+[[ ${lineOrder} = *"ssloff"* ]] && echo 'ssl not configured on domains' || ./sslConfig.sh
+if [[ $autoRenew == 1 ]]; then
+    crontab -l > mycron
+    [[ ! $(sed -e '/certbot-auto/ !d' mycron) ]] && echo "45 2 * * 6 /root/.startup/certbot-auto renew && cp /etc/letsencrypt/live/$mainDomain/*.* /app/nginx/letsencrypt && docker restart $dockerNginxContainer" >> mycron
+    crontab mycron
+    rm mycron
+    echo 'Added weekly task to cron'
+fi
+#
+docker start nginx
+echo -en "\n\n"$LINE"\n\t"
 read -rsn1 -p "Press any key to continue > "
-echo
-exit 1
+exit 0
